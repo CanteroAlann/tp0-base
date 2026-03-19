055b1ffe66776c49dc2ee8d69d6d0727687b4ab3
alan <acantero@fi.uba.ar>
2026-03-19 03:32:37 -0300

    fix: error on script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 SERVER_IP="172.25.125.2"
 SERVER_PORT="12345"
 MESSAGE="testing_echo_service"
@@ -8,7 +7,7 @@ MESSAGE="testing_echo_service"
 
 RESPONSE=$(echo "$MESSAGE" | nc -w 2 "$SERVER_IP" "$SERVER_PORT" 2>/dev/null)
 
-if [ "$RESPONSE" == "$MESSAGE" ]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
     exit 0
 else
